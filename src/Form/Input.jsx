930374994287a16c8d5f7fb43e19cb2b51199b59
jasu55 <jasu14707@gmail.com>
2025-09-01 15:18:59 +0800

    9.1 sur

---
+++ src/Form/Input.jsx
@@ -0,0 +1,10 @@
+export function Input() {
+  return (
+    <div>
+      <input
+        className="border outline-[#0CA5E9] border-[#CBD5E1] my-[8px] rounded-md text-[16px] h- w-full text-[#8B8E95] p-[12px]"
+        placeholder="Placeholder "
+      />
+    </div>
+  );
+}
